12b9e87236328cfc46017e3aaa86a4a6f8fc1d7c
huanghongkai <kai@rayvision.com>
2020-10-23 22:11:50 +0800

    :sparkles: 安装依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, Space, Table } from 'antd'
+import { ColumnProps } from 'antd/lib/table'
+import './App.css'
 
+interface Entity {
+  id: number
+  name: string
+}
+const data: Entity[] = [
+  { id: 1, name: 'one' },
+  { id: 2, name: 'two' },
+]
+const columns: ColumnProps<Entity>[] = [
+  { dataIndex: 'id', title: 'id' },
+  { dataIndex: 'name', title: 'name' },
+  {
+    render: () => (
+      <Space>
+        <Button type='text' size='small'>
+          编辑
+        </Button>
+        <Button type='text' danger>
+          删除
+        </Button>
+      </Space>
+    ),
+  },
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>antd table crud</h3>
+      <Table dataSource={data} columns={columns}></Table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
