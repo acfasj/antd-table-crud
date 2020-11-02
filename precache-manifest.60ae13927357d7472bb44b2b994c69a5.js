64c5d59c13b17dadfdc4175c186380f69c9daf89
acfasj <acfasj@users.noreply.github.com>
2020-11-02 06:51:54 +0000

    deploy: 689b82484e70368312c41c8f3ef90b6ba1461b7b

---
+++ precache-manifest.60ae13927357d7472bb44b2b994c69a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621da34eb6d602bce631f5109fb8911c",
+    "url": "/antd-table-crud/index.html"
+  },
+  {
+    "revision": "510f32885474a9f830e8",
+    "url": "/antd-table-crud/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "3817edcc9ced2d5f2f9f",
+    "url": "/antd-table-crud/static/css/main.14a037df.chunk.css"
+  },
+  {
+    "revision": "510f32885474a9f830e8",
+    "url": "/antd-table-crud/static/js/2.678576d1.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/antd-table-crud/static/js/2.678576d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3817edcc9ced2d5f2f9f",
+    "url": "/antd-table-crud/static/js/main.3947d70c.chunk.js"
+  },
+  {
+    "revision": "5cb59da97347888be30e",
+    "url": "/antd-table-crud/static/js/runtime-main.bac6b46f.js"
+  }
+]);
